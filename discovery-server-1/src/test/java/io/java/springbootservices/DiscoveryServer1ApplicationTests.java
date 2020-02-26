45eb5776e82d576274b83da55b12602909e3787a
gmanigandan16 <gmanigandan16@gmail.com>
2020-02-26 23:36:34 +0530

    Discovery server re-written

---
+++ discovery-server-1/src/test/java/io/java/springbootservices/DiscoveryServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.springbootservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
